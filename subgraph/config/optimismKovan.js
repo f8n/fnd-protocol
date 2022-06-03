6d34b4b2945ce5cbbf161552863df432c13cb8cc
Batu Inal <batuinal93@gmail.com>
2022-06-03 11:57:35 -0700

    Revert "update op Kovan Subgraph Block Number"
    This reverts commit ecb642e2f8592a314dd81de7edccc1ca1703bba6.

--- subgraph/config/optimismKovan.js
+++ subgraph/config/optimismKovan.js
@@ -5,12 +5,12 @@ const getAddressesForSubgraph = require("./shared");
 module.exports = {
   network: "optimism-kovan",
   startBlock: {
-    treasury: 31983739,
-    nftMarket: 31983739,
-    percentSplitV2: 31983739,
-    percentSplit: 31983739,
-    collectionFactory: 31983739,
-    feth: 31983739,
+    treasury: 238000,
+    nftMarket: 238000,
+    percentSplitV2: 238000,
+    percentSplit: 238000,
+    collectionFactory: 238000,
+    feth: 238000,
   },
   ...getAddressesForSubgraph(addresses.staging[69]),
 };
