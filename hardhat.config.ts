5984f2110f673711bb3020cee7612a98c9ccb614
Nick Mancuso <hardlydiff@gmail.com>
2022-06-21 07:08:06 -0700

    Bump solc to 0.8.15 (#136)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,7 @@ import "hardhat-tracer";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.13",
+    version: "0.8.15",
     settings: {
       optimizer: {
         enabled: true,
